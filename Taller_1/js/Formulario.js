586fe78d5452c6c85c2badca37dfd11ccf04464a
Camilodez <hernandezz_camilo@javeriana.edu.co>
2024-02-06 11:07:07 -0500

    update full

---
+++ Taller_1/js/Formulario.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function()  {
+    const friendForm = document.getElementById('friendForm');
+    const usernameInput = document.getElementById('username');
+    const nameInput = document.getElementById('name');
+    const imageURLInput = document.getElementById('imageURL');
+    const petsInput = document.getElementById('pets');
+    const friendTableBody = document.querySelector("#friendTable tbody");
+    const petsList = document.getElementById("petsList");
+
+    const ArrayFriends = [];
+    
+})
